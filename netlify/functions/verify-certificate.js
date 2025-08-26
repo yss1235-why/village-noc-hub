ce0c9aa72a4d71bebc5a54e502ea80ab913cd524
yss1235-why <cofclanone1@gmail.com>
2025-08-26 16:09:17 +0530

    Update verify-certificate.js

--- netlify/functions/verify-certificate.js
+++ netlify/functions/verify-certificate.js
@@ -36,30 +36,26 @@ console.log('Searching for application:', applicationNumber);
 
 // Get application data with village info
 const application = await sql`
-      SELECT 
-        a.application_number,
-        a.applicant_name,
-        a.title,
-        a.relation,
-        a.father_name,
-        a.mother_name,
-        a.husband_name,
-        a.guardian_name,
-        a.tribe_name,
-        a.religion,
-        a.annual_income,
-        a.status,
-        a.approved_date,
-        a.created_at,
-        v.name as village_name,
-        v.district,
-        v.state,
-        v.admin_name
-      FROM noc_applications a
-      JOIN villages v ON a.village_id::uuid = v.id
-      WHERE a.application_number = ${applicationNumber}
-      AND a.status = 'approved'
-    `;
+  SELECT 
+    a.application_number,
+    a.applicant_name,
+    a.title,
+    a.relation,
+    a.tribe_name,
+    a.religion,
+    a.annual_income,
+    a.status,
+    a.approved_date,
+    a.created_at,
+    v.name as village_name,
+    v.district,
+    v.state,
+    v.admin_name
+  FROM noc_applications a
+  JOIN villages v ON a.village_id::uuid = v.id
+  WHERE a.application_number = ${applicationNumber}
+  AND a.status = 'approved'
+`;
 
     if (!application.length) {
       return {
@@ -74,17 +70,8 @@ const application = await sql`
 
     const app = application[0];
 
-    // Build relation string
-    let relationString = '';
-    if (app.relation && (app.father_name || app.mother_name || app.husband_name || app.guardian_name)) {
-      const relationName = app.father_name || app.mother_name || app.husband_name || app.guardian_name;
-      const relationPrefix = app.relation === 'S/O' ? 'Son of' : 
-                           app.relation === 'D/O' ? 'Daughter of' : 
-                           app.relation === 'W/O' ? 'Wife of' : 
-                           app.relation === 'H/O' ? 'Husband of' : 
-                           app.relation;
-      relationString = `${relationPrefix} ${relationName}`;
-    }
+   // Use relation field as-is (whatever was entered in the form)
+let relationString = app.relation || '';
 
     return {
       statusCode: 200,
