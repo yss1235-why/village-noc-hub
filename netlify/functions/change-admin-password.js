10e233005c0572e6538a408fa9da84b8ab56ee75
yss1235-why <cofclanone1@gmail.com>
2025-09-15 19:44:47 +0530

    Update change-admin-password.js

--- netlify/functions/change-admin-password.js
+++ netlify/functions/change-admin-password.js
@@ -1,7 +1,10 @@
-import { sql } from './utils/db.js';
-import bcrypt from 'bcrypt';
 
+
+
+import { neon } from '@neondatabase/serverless';
+import bcrypt from 'bcrypt';
 export const handler = async (event, context) => {
+  const sql = neon(process.env.NETLIFY_DATABASE_URL);
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type',
