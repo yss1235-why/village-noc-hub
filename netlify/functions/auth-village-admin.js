3b31044329fac3988351383b5b092c75fb85d581
yss1235-why <cofclanone1@gmail.com>
2025-09-15 16:37:56 +0530

    Update auth-village-admin.js

--- netlify/functions/auth-village-admin.js
+++ netlify/functions/auth-village-admin.js
@@ -1,7 +1,8 @@
-import { sql } from './utils/db.js';
+const { sql } = require('./utils/db.js');
+
 import bcrypt from 'bcrypt';
 
-export const handler = async (event, context) => {
+exports.handler = async (event, context) => {
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type',
