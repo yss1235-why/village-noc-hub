5bd75119d8e162951b4e8dde4be643508b95b5ab
yss1235-why <cofclanone1@gmail.com>
2025-09-27 23:33:11 +0530

    Update redeem-voucher.js

--- netlify/functions/redeem-voucher.js
+++ netlify/functions/redeem-voucher.js
@@ -2,7 +2,7 @@ import { neon } from '@neondatabase/serverless';
 import crypto from 'crypto';
 import { requireRole } from './utils/auth-middleware.js';
 import { validateVoucherConfig } from './utils/voucher-config.js';
-
+import { deterministicStringify } from './utils/deterministic-stringify.js';
 // Helper function to extract single IP address from x-forwarded-for header
 const getClientIP = (headers) => {
   const forwarded = headers['x-forwarded-for'];
