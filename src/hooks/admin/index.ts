71e0821e5624f11c68336c88c7faca85acc88963
yss1235-why <cofclanone1@gmail.com>
2025-12-09 18:39:16 +0530

    Create index.ts

---
+++ src/hooks/admin/index.ts
@@ -0,0 +1,8 @@
+export { useAdminApplications } from './useAdminApplications';
+export type { Application } from './useAdminApplications';
+
+export { useAdminDocuments } from './useAdminDocuments';
+export type { Documents, DocumentFiles } from './useAdminDocuments';
+
+export { useAdminProfile } from './useAdminProfile';
+export type { ProfileForm, VillageForm, PasswordForm } from './useAdminProfile';
